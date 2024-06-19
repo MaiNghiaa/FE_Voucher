f04d99566652da51ea4ac04396f6f214ee78ba09
Trung Nghĩa <109063567+MaiNghiaa@users.noreply.github.com>
2024-06-19 10:48:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import VoucherManagement from './Pages/VoucherManagement/VoucherManagement';
+import logo from "./logo.svg";
+import "./App.css";
+import VoucherManagement from "./Pages/VoucherManagement/VoucherManagement";
+import VoucherCampaignManagement from "./Pages/VoucherCampaignManagement/VoucherCampaignManagement";
 
 function App() {
   return (
     <div className="App">
-      <VoucherManagement />
+      {/* <VoucherManagement /> */}
+      <VoucherCampaignManagement />
     </div>
   );
 }
