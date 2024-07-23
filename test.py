efc4de1f7fde849087fe5dfae045d179cff25021
Trung Nghĩa <109063567+MaiNghiaa@users.noreply.github.com>
2024-07-23 08:36:49 +0700

    Update Nhiem vu

--- test.py
+++ test.py
@@ -1,22 +1,11 @@
-def solution(n):
-    if n % 2 != 0:
-        return 0
-    if n == 0:
-        return 1 
-    if n == 2:
-        return 1
+#1 sửa các button ở sidebar 
 
-    dp = [0] * (n + 1)
-    dp[0] = 1  # Base case: container 4x0
-    dp[1] = 0  # No way to fill a 4x1 container with 1x2 blocks
-    dp[2] = 1  # Base case: container 4x2
+#2 Sửa các cái input, Calendar đúng ( đúng UI)
+#2.1 Calendar, Tang giam so luong o sanpham
 
-    # Fill dp array for all even lengths
-    for i in range(4, n + 1, 2):
-        dp[i] = dp[i - 2] + 2 * sum(dp[i - j] for j in range(4, i, 2))
+# Nhiem vu tối 
+# Chức năng sửa và xóa sản phẩm
+# Cần ấn ra ngoài để đóng các form
+#4 Sửa ảnh
+# Input Hangmuc, Trang thai,
 
-    return dp[n]
-
-# Test the function
-print("solution(1) =", solution(1))  # Output should be 1
-print("solution(4) =", solution(4))  # Output should be 36
